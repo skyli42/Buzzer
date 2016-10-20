89c178c66dae8e5c09694d22255d56a8d242da96
Sky Li <skylimail42@gmail.com>
2016-10-19 23:32:10 -0400

    first

---
+++ index.js
@@ -0,0 +1,34 @@
+var express=require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var players=new Map();
+app.use(express.static("./assets"));
+app.get('/', function(req, res){
+  res.sendFile(__dirname+ '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('new player', function(msg){
+  	players.set(socket.client.id, msg);
+  	console.log(socket.client.id, msg)
+  })
+  socket.on('buzz', function(msg){
+  	buzz(msg);
+  })
+  socket.on('reset', function(){
+  	console.log('reset')
+  	io.emit('reset');
+  })
+});
+var buzz=function(id){
+	console.log(players.get(id))
+	io.emit('buzz', players.get(id));
+	console.log(id)
+}
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
