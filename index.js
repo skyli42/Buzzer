5adadd18bb87d0d660c76cd1a9e02de62c1a087d
Sky Li <skylimail42@gmail.com>
2016-10-19 23:48:06 -0400

    hopefully last fix

--- index.js
+++ index.js
@@ -15,20 +15,18 @@ io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('new player', function(msg){
   	players.set(socket.client.id, msg);
-  	console.log(socket.client.id, msg)
+  	// console.log(socket.client.id, msg)
   })
   socket.on('buzz', function(msg){
   	buzz(msg);
   })
   socket.on('reset', function(){
-  	console.log('reset')
   	io.emit('reset');
   })
 });
 var buzz=function(id){
-	console.log(players.get(id))
+	// console.log(players.get(id))
 	io.emit('buzz', players.get(id));
-	console.log(id)
 }
 
 server.listen(app.listen(process.env.PORT || 3000, function() {
