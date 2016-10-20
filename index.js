35674328cfd3d6186d71af781ee5ee38a01c924b
Sky Li <skylimail42@gmail.com>
2016-10-20 18:40:27 -0400

    fixed interface

--- index.js
+++ index.js
@@ -8,29 +8,44 @@ var players=new Map();
 
 app.use(express.static("./assets"));
 app.get('/', function(req, res){
-  res.sendFile(__dirname+ '/index.html');
+	res.sendFile(__dirname+ '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('new player', function(msg){
-  	players.set(socket.client.id, msg);
-  	// console.log(socket.client.id, msg)
-  })
-  socket.on('buzz', function(msg){
-  	buzz(msg);
-  })
-  socket.on('reset', function(){
-  	io.emit('reset');
-  })
+	console.log('a user connected');
+	socket.on('new player', function(msg){
+		console.log("connect")
+		players.set(socket.client.id, msg);
+		var send={
+			id:players.get(socket.client.id),
+			arr:Array.from(players.values())
+		}
+		io.emit('new connection', send);
+	})
+	socket.on('buzz', function(msg){
+		buzz(msg);
+	})
+	socket.on('reset', function(){
+		io.emit('reset');
+	})
+	socket.on('disconnect', function(){
+		var dis= players.get(socket.client.id);
+		players.delete(socket.client.id);
+		var send={
+			id:dis,
+			arr:Array.from(players.values())
+		}
+		// io.emit('new connection', Array.from(players.values()));
+		io.emit('disconnection', send);
+	})
 });
+
 var buzz=function(id){
-	// console.log(players.get(id))
 	io.emit('buzz', players.get(id));
 }
 
 server.listen(app.listen(process.env.PORT || 3000, function() {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Listening at http://%s:%s', host, port);
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('Listening at http://%s:%s', host, port);
 }));
