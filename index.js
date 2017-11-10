584f080b01a4f8a6d552d2e1e22c7176433bdff7
Sky Li <skylimail42@gmail.com>
2017-11-09 21:19:14 -0500

    fixed?

--- index.js
+++ index.js
@@ -48,4 +48,4 @@ if(process.env.PORT != undefined){
 	listenPort = process.env.PORT;
 }
 
-app.listen(process.env.PORT||3000, () => console.log('Example app listening on port 3000!'))
+// app.listen(process.env.PORT||3000, () => console.log('Example app listening on port 3000!'))
