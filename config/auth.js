97c8f3c23a047fa47b3ea1545550e0cf6918d7bc
Sky Li <skylimail42@gmail.com>
2018-01-14 23:04:50 -0500

    bug fixes

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
     'googleAuth' : {
         'clientID'      : '133795350687-eiepqktsbhqmbj0dtp9264ukprtu9eqt.apps.googleusercontent.com',
         'clientSecret'  : 'iAlP21dKQrf8ZHvRVcnSjoM2',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+        'callbackURL'   : 'http://www.qbbuzz.me/auth/google/callback'
     }
 
 };
