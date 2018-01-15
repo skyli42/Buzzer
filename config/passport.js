9eb115c4a8cd0b3982cf477ffef605f174d74a40
Sky Li <skylimail42@gmail.com>
2018-01-14 23:11:35 -0500

    bug fixes

--- config/passport.js
+++ config/passport.js
@@ -1,10 +1,10 @@
-var LocalStrategy   = require('passport-local').Strategy;
+var LocalStrategy = require('passport-local').Strategy;
 
 // load up the user model
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 // load up the user model
-var User       = require('../app/models/user');
+var User = require('../app/models/user');
 
 // load the auth variables
 var configAuth = require('./auth');
@@ -30,62 +30,62 @@ module.exports = function(passport) {
     // by default, if there was no name, it would just be called 'local'
 
     passport.use('local-signup', new LocalStrategy({
-        // by default, local strategy uses username and password, we will override with email
-        usernameField : 'email',
-        passwordField : 'password',
-        passReqToCallback : true // allows us to pass back the entire request to the callback
-    },
-    function(req, email, password, done) {
+            // by default, local strategy uses username and password, we will override with email
+            usernameField: 'email',
+            passwordField: 'password',
+            passReqToCallback: true // allows us to pass back the entire request to the callback
+        },
+        function(req, email, password, done) {
 
-        // asynchronous
-        // User.findOne wont fire unless data is sent back
-        process.nextTick(function() {
+            // asynchronous
+            // User.findOne wont fire unless data is sent back
+            process.nextTick(function() {
 
-        // find a user whose email is the same as the forms email
-        // we are checking to see if the user trying to login already exists
-        User.findOne({ 'local.email' :  email }, function(err, user) {
-            // if there are any errors, return the error
-            if (err)
-                return done(err);
-
-            // check to see if theres already a user with that email
-            if (user) {
-                return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-            } else {
-
-                // if there is no user with that email
-                // create the user
-                var newUser            = new User();
-
-                // set the user's local credentials
-                newUser.local.email    = email;
-                newUser.local.password = newUser.generateHash(password);
-                newUser.points = 0
-                newUser.username = email
-                // save the user
-                newUser.save(function(err) {
+                // find a user whose email is the same as the forms email
+                // we are checking to see if the user trying to login already exists
+                User.findOne({ 'local.email': email }, function(err, user) {
+                    // if there are any errors, return the error
                     if (err)
-                        throw err;
-                    return done(null, newUser);
+                        return done(err);
+
+                    // check to see if theres already a user with that email
+                    if (user) {
+                        return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+                    } else {
+
+                        // if there is no user with that email
+                        // create the user
+                        var newUser = new User();
+
+                        // set the user's local credentials
+                        newUser.local.email = email;
+                        newUser.local.password = newUser.generateHash(password);
+                        newUser.points = 0
+                        newUser.username = email
+                        // save the user
+                        newUser.save(function(err) {
+                            if (err)
+                                throw err;
+                            return done(null, newUser);
+                        });
+                    }
+
                 });
-            }
 
-        });    
+            });
 
-    });
-
-    }));
+        }));
     passport.use('local-login', new LocalStrategy({
             // by default, local strategy uses username and password, we will override with email
-            usernameField : 'email',
-            passwordField : 'password',
-            passReqToCallback : true // allows us to pass back the entire request to the callback
+            usernameField: 'email',
+            passwordField: 'password',
+            passReqToCallback: true // allows us to pass back the entire request to the callback
         },
         function(req, email, password, done) { // callback with email and password from our form
 
             // find a user whose email is the same as the forms email
             // we are checking to see if the user trying to login already exists
-            User.findOne({ 'local.email' :  email }, function(err, user) {
+            User.findOne({ 'local.email': email }, function(err, user) {
                 // if there are any errors, return the error before anything else
                 if (err)
                     return done(err);
@@ -105,70 +105,70 @@ module.exports = function(passport) {
         }));
     passport.use(
         new GoogleStrategy({
-            clientID        : configAuth.googleAuth.clientID,
-            clientSecret    : configAuth.googleAuth.clientSecret,
-            callbackURL     : configAuth.googleAuth.callbackURL,
-            passReqToCallback:true
-        }, 
-        function(req, token, refreshToken, profile, done) {
-            process.nextTick(function() {
-                if(!req.user){
-                    // try to find the user based on their google id
-                    User.findOne({ 'google.id' : profile.id }, function(err, user) {
-                       if (err)
-                           return done(err);
+                clientID: configAuth.googleAuth.clientID,
+                clientSecret: configAuth.googleAuth.clientSecret,
+                callbackURL: configAuth.googleAuth.callbackURL,
+                passReqToCallback: true
+            },
+            function(req, token, refreshToken, profile, done) {
+                process.nextTick(function() {
+                    if (!req.user) {
+                        // try to find the user based on their google id
+                        User.findOne({ 'google.id': profile.id }, function(err, user) {
+                            if (err)
+                                return done(err);
 
-                       if (user) {
-                        if (!user.google.token) {
-                            user.google.token = token;
-                            user.google.name  = profile.name.givenName + ' ' + profile.name.familyName;
-                            user.google.email = profile.emails[0].value;
+                            if (user) {
+                                if (!user.google.token) {
+                                    user.google.token = token;
+                                    user.google.name = profile.name.givenName + ' ' + profile.name.familyName;
+                                    user.google.email = profile.emails[0].value;
 
-                            user.save(function(err) {
-                                if (err)
-                                    throw err;
+                                    user.save(function(err) {
+                                        if (err)
+                                            throw err;
+                                        return done(null, user);
+                                    });
+                                } // if a user is found, log them in
                                 return done(null, user);
-                            });
-                                                    }// if a user is found, log them in
-                                                    return done(null, user);
-                                                } else {
-                             // if the user isnt in our database, create a new user
-                             var newUser          = new User();
+                            } else {
+                                // if the user isnt in our database, create a new user
+                                var newUser = new User();
 
-                             // set all of the relevant information
-                             newUser.google.id    = profile.id;
-                             newUser.google.token = token;
-                             newUser.google.name  = profile.displayName;
-                             newUser.google.email = profile.emails[0].value; // pull the first email
+                                // set all of the relevant information
+                                newUser.google.id = profile.id;
+                                newUser.google.token = token;
+                                newUser.google.name = profile.displayName;
+                                newUser.google.email = profile.emails[0].value; // pull the first email
+                                newUser.points = 0
+                                newUser.username = email
+                                // save the user
+                                newUser.save(function(err) {
+                                    if (err)
+                                        throw err;
+                                    return done(null, newUser);
+                                });
+                            }
+                        });
+                    } else {
+                        var user = req.user; // pull the user out of the session
 
-                             // save the user
-                             newUser.save(function(err) {
-                               if (err)
-                                   throw err;
-                               return done(null, newUser);
-                           });
-                         }
-                     });
-                }
-                else{
-                    var user = req.user; // pull the user out of the session
+                        // update the current users google credentials
+                        user.google.id = profile.id;
+                        user.google.token = token;
+                        user.google.name = profile.name.givenName + ' ' + profile.name.familyName;
+                        user.google.email = profile.emails[0].value;
+                        
+                        // save the user
+                        user.save(function(err) {
+                            if (err)
+                                throw err;
+                            return done(null, user);
+                        });
+                    }
+                });
 
-                    // update the current users google credentials
-                    user.google.id    = profile.id;
-                    user.google.token = token;
-                    user.google.name  = profile.name.givenName + ' ' + profile.name.familyName;
-                    user.google.email = profile.emails[0].value;
 
-                    // save the user
-                    user.save(function(err) {
-                        if (err)
-                            throw err;
-                        return done(null, user);
-                    });
-                }
-            });
-            
-
-        })
-        );
+            })
+    );
 };
