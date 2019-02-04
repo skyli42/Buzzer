d7dc530a397f80c97d577a445de024cd6cfc9299
Sky Li <skylimail42@gmail.com>
2019-02-04 17:34:09 -0500

    cleanup

--- config/auth.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'googleAuth' : {
-        'clientID'      : '133795350687-eiepqktsbhqmbj0dtp9264ukprtu9eqt.apps.googleusercontent.com',
-        'clientSecret'  : 'iAlP21dKQrf8ZHvRVcnSjoM2',
-        'callbackURL'   : 'http://www.qbbuzz.me/auth/google/callback'
-    }
-
-};
